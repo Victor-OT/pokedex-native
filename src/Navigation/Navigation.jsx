cefae1f0d2c014aad352254b31384d40d9a4cbbe
Victor-OT <crowbreaker96@hotmail.com>
2024-08-09 11:29:28 -0600

    Se personalizó el tab navigation, para lo que se importó un icono de pokebola, y se añadieron respectivos iconos a cada tab screen

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -1,18 +1,52 @@
-import { View, Text } from 'react-native'
+import { Image } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-vector-icons/FontAwesome5'
 import Account from '../Screens/Account'
-import Favorites from '../Screens/Favorites'
 import Pokedex from '../Screens/Pokedex'
+import Favorites from '../Screens/Favorites'
 
 export default function Navigation() {
     const Tab = createBottomTabNavigator()
+    function renderPokeballIcon () {
+      return(
+        <Image 
+          source={require('../assets/pokeball.png')}
+          style={{width: 75, height: 75, top:-15}}/>
+      )
+    }
 
   return (
-    <Tab.Navigator>
-        <Tab.Screen name='Pokedex' component={Pokedex}/>
-        <Tab.Screen name='Favorites' component={Favorites}/>
-        <Tab.Screen name='Account' component={Account}/>
+    <Tab.Navigator screenOptions={{headerTitleAlign: 'center'}}>
+        <Tab.Screen 
+          name='Favorites' 
+          component={Favorites} 
+          options={{
+            tabBarLabel: 'Favoritos',
+            tabBarIcon: ({ color,size }) => (
+              <Icon name='heart' color={color} size={size}/>
+            )
+          }}/>
+
+        <Tab.Screen 
+          name='Pokedex' 
+          component={Pokedex}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: () => (
+              renderPokeballIcon()
+            )
+          }}/>
+
+        <Tab.Screen 
+          name='Account' 
+          component={Account}
+          options={{
+            tabBarLabel: 'Mi Cuenta',
+            tabBarIcon: ({color, size}) => (
+              <Icon name='user' color={color} size={size}/>
+            )
+          }}/>
     </Tab.Navigator>
   )
 }
