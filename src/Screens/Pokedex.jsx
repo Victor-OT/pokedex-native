93fea2d93816876cb5457017dcfac3c83c317668
Victor-OT <crowbreaker96@hotmail.com>
2024-08-09 11:33:37 -0600

    Se creó screen Pokemon y se importó en Pokedex

--- src/Screens/Pokedex.jsx
+++ src/Screens/Pokedex.jsx
@@ -1,10 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Pokemon from "./Pokemon";
 
 export default function Pokedex () {
+    const Stack = createNativeStackNavigator()
     return (
-        <View>
-            <Text>Pokedex</Text>
-        </View>
+        <Stack.Navigator>
+            <Stack.Screen name="Pokemon" component={Pokemon}/>
+        </Stack.Navigator>
     )
 }
